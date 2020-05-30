1d7d1a46872cafd3bc4fe7db1565d09880395298
Moshe Gotam <56716267+MosheG23@users.noreply.github.com>
2020-05-30 16:32:47 +0300

    portfolio more focused

--- js/scripts.js
+++ js/scripts.js
@@ -117,13 +117,15 @@ for (var i = 0; i < btns.length; i++) {
 }
 
 //////////////////////////////////////////////////////////////////////
-  ///////////////////////// images slideshow ////////////////////////
+///////////////////////// images slideshow ////////////////////////
 /////////////////////////////////////////////////////////////////////
 var slideIndex = 1;
+var projectName = "";
 showSlides(slideIndex);
 
 // Next/previous controls
 function plusSlides(n) {
+  console.log(slideIndex);
   showSlides(slideIndex += n);
 }
 
@@ -134,10 +136,32 @@ function currentSlide(n) {
 
 function showSlides(n) {
   var i;
-  var slides = document.getElementsByClassName("mySlides");
-  var dots = document.getElementsByClassName("dot");
-  if (n > slides.length) { slideIndex = 1 }
-  if (n < 1) { slideIndex = slides.length }
+  var project = document.getElementById(projectName);
+  var slides = project.getElementsByClassName("mySlides");
+  var dots = project.getElementsByClassName("dot");
+  if (n > slides.length) {
+    slideIndex = 1
+  }
+  if (n < 1) {
+    slideIndex = slides.length
+  }
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex - 1].style.display = "block";
+  dots[slideIndex - 1].className += " active";
+}
+
+function showSlidesProject(projectN) {
+  var i;
+  projectName = projectN;
+  var project = document.getElementById(projectName);
+  var slides = project.getElementsByClassName("mySlides");
+  var dots = project.getElementsByClassName("dot");
+  slideIndex = 1;
   for (i = 0; i < slides.length; i++) {
     slides[i].style.display = "none";
   }
