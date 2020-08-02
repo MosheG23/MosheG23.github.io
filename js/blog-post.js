d89a33ee610b73cc7245b2ba0a2b43d5e60cb4b8
Moshe Gotam <56716267+MosheG23@users.noreply.github.com>
2020-08-02 19:01:33 +0300

    gifForDarkMode_ResumeModalStart

--- js/blog-post.js
+++ js/blog-post.js
@@ -47,3 +47,8 @@ function readingTime(text) {
 }
 
 readingTime(text);
+
+document.addEventListener('DOMContentLoaded', function () {
+    var elems = document.querySelectorAll('.fixed-action-btn');
+    var instances = M.FloatingActionButton.init(elems, options);
+});
