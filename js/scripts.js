aad1208d6ba9a0b843808653fb974aaba7f759cc
Moshe Gotam <56716267+MosheG23@users.noreply.github.com>
2020-09-22 22:53:53 +0300

    change blog layout, project section

--- js/scripts.js
+++ js/scripts.js
@@ -115,53 +115,53 @@
   })(jQuery); // End of use strict
 
 
-//////////////////////////////////////////////////////////////////////
-  ///////////////// filter images by subject ////////////////////////
-/////////////////////////////////////////////////////////////////////
-filterSelection("all")
-function filterSelection(c) {
-  var x, i;
-  x = document.getElementsByClassName("col-md-6 col-lg-4 mb-5");
-  if (c == "all") c = "";
-  for (i = 0; i < x.length; i++) {
-    console.log(x);
-    w3RemoveClass(x[i], "show");
-    if (x[i].className.indexOf(c) > -1) w3AddClass(x[i], " portfolio-show");
-  }
-}
+// //////////////////////////////////////////////////////////////////////
+//   ///////////////// filter images by subject ////////////////////////
+// /////////////////////////////////////////////////////////////////////
+// filterSelection("all")
+// function filterSelection(c) {
+//   var x, i;
+//   x = document.getElementsByClassName("col-md-6 col-lg-4 mb-5");
+//   if (c == "all") c = "";
+//   for (i = 0; i < x.length; i++) {
+//     console.log(x);
+//     w3RemoveClass(x[i], "show");
+//     if (x[i].className.indexOf(c) > -1) w3AddClass(x[i], " portfolio-show");
+//   }
+// }
 
-function w3AddClass(element, name) {
-  var i, arr1, arr2;
-  arr1 = element.className.split(" ");
-  arr2 = name.split(" ");
-  for (i = 0; i < arr2.length; i++) {
-    if (arr1.indexOf(arr2[i]) == -1) { element.className += " " + arr2[i]; }
-  }
-}
+// function w3AddClass(element, name) {
+//   var i, arr1, arr2;
+//   arr1 = element.className.split(" ");
+//   arr2 = name.split(" ");
+//   for (i = 0; i < arr2.length; i++) {
+//     if (arr1.indexOf(arr2[i]) == -1) { element.className += " " + arr2[i]; }
+//   }
+// }
 
-function w3RemoveClass(element, name) {
-  var i, arr1, arr2;
-  arr1 = element.className.split(" ");
-  arr2 = name.split(" ");
-  for (i = 0; i < arr2.length; i++) {
-    while (arr1.indexOf(arr2[i]) > -1) {
-      arr1.splice(arr1.indexOf(arr2[i]), 1);
-    }
-  }
-  element.className = arr1.join(" ");
-}
+// function w3RemoveClass(element, name) {
+//   var i, arr1, arr2;
+//   arr1 = element.className.split(" ");
+//   arr2 = name.split(" ");
+//   for (i = 0; i < arr2.length; i++) {
+//     while (arr1.indexOf(arr2[i]) > -1) {
+//       arr1.splice(arr1.indexOf(arr2[i]), 1);
+//     }
+//   }
+//   element.className = arr1.join(" ");
+// }
 
 
-// Add active class to the current button (highlight it)
-var btnContainer = document.getElementById("btnPortFilter");
-var btns = btnContainer.getElementsByClassName("btn");
-for (var i = 0; i < btns.length; i++) {
-  btns[i].addEventListener("click", function () {
-    var current = btnContainer.getElementsByClassName("active");
-    current[0].className = current[0].className.replace(" active", "");
-    this.className += " active";
-  });
-}
+// // Add active class to the current button (highlight it)
+// var btnContainer = document.getElementById("btnPortFilter");
+// var btns = btnContainer.getElementsByClassName("btn");
+// for (var i = 0; i < btns.length; i++) {
+//   btns[i].addEventListener("click", function () {
+//     var current = btnContainer.getElementsByClassName("active");
+//     current[0].className = current[0].className.replace(" active", "");
+//     this.className += " active";
+//   });
+// }
 
 //////////////////////////////////////////////////////////////////////
 ///////////////////////// images slideshow ////////////////////////
