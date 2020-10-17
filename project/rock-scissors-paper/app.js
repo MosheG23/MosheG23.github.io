e6825c793176a21dc72d85504ce4d6a92781245d
Moshe Gotam <56716267+MosheG23@users.noreply.github.com>
2020-10-17 20:49:42 +0300

    total makeover

---
+++ project/rock-scissors-paper/app.js
@@ -0,0 +1,12 @@
+new fullpage("#fullpage", {
+    autoScrolling: true,
+    navigation: true,
+    scrollHorizontally: true,
+    fitToSection: true,
+    scrollOverflow: true, 
+    loopBottom: true,
+    navigationTooltips: ['Overview', 'Rules', 'Modes', 'App Map'],
+    showActiveTooltip: true, 
+	paddingTop: '3em',
+	paddingBottom: '3em',
+})
